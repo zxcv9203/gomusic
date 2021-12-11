87cd6079eb3c13a9b828a7b0550b1702eae205d1
yongckim <zxcv9203@naver.com>
2021-12-11 22:28:25 +0900

    db interface create

--- backend/src/dblayer/dblayer.go
+++ backend/src/dblayer/dblayer.go
@@ -1,7 +1,15 @@
 package dblayer
 
-import "github.com/zxcv9203/gomusic/backend/src/models"
+import "models"
 
 type DBLayer interface {
 	GetAllProducts() ([]models.Product, error)
+	GetPromos() ([]models.Product, error)
+	GetCustomerByName(string, string) (models.Customer, error)
+	GetCustomerByID(int) (models.Customer, error)
+	GetProduct(uint) (models.Product, error)
+	AddUser(models.Customer) (models.Customer, error)
+	SignInUser(username, password string) (models.Customer, error)
+	SignOutUserById(int) error
+	GetCustomerOrdersById(int) ([]models.Order, error)
 }
