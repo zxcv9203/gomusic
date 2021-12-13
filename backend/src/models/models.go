c159e3472edcef7342ffb91217320dcd8a1c2510
yongckim <zxcv9203@naver.com>
2021-12-14 04:13:32 +0900

    gorm model modify & gorm install

--- backend/src/models/models.go
+++ backend/src/models/models.go
@@ -1,29 +1,45 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Product struct {
+	gorm.Model
 	Image       string  `json:"img"`
-	ImagAlt     string  `json:"imgalt"`
+	ImagAlt     string  `gorm:"column:imgalt" json:"imgalt"`
 	Price       float64 `json:"price"`
-	Promotion   float64 `json:"promotion"`
-	ProductName string  `json:"productname"`
-	Description string  `json:"desc"`
+	Promotion   float64 `json:"promotion"` // sql.NullFloat64
+	ProductName string  `gorm:"column:productname" json:"productname"`
+	Description string
+}
+
+func (Product) TableName() string {
+	return "products"
 }
 
 type Customer struct {
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	Email     string `json:"email"`
-	Pass      string `json:"password"`
-	LoggedIn  bool   `json:"loggedin"`
+	gorm.Model
+	FirstName string  `gorm:"column:firstname" json:"firstname"`
+	LastName  string  `gorm:"column:lastname" json:"lastname"`
+	Email     string  `gorm:"column:email" json:"email"`
+	Pass      string  `json:"password"`
+	LoggedIn  bool    `gorm:"column:loggedin" json:"loggedin"`
+	Orders    []Order `json:"orders"`
 }
 
 type Order struct {
+	gorm.Model
 	Product
 	Customer
-	CustomerId   int       `json:"customer_id"`
-	ProductId    int       `json:"product_id"`
-	Price        float64   `json:"sell_price"`
-	PurchaseDate time.Time `json:"purchase_date"`
+	CustomerId   int       `gorm:"column:customer_id"`
+	ProductId    int       `gorm:"column:product_id"`
+	Price        float64   `gorm:"column:price" json:"sell_price"`
+	PurchaseDate time.Time `gorm:"column:purchase_date" json:"purchase_date"`
+}
+
+func (Order) TableName() string {
+	return "orders"
 }
